e942b362dd5a9e01b9cfa58fb8002526ba017719
Lucas Kerbs <lucaskerbs@gmail.com>
2025-07-11 13:25:55 -0500

    modululize telescope

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -12,7 +12,9 @@ return {
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
-    picker = { enabled = true },
+    picker = {
+      enabled = true
+    },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
